104ec85cd1ede89abaca6cb0aa5c37b7648ac6c8
Diego S. Seabra <dss-diego@hotmail.com>
2026-01-13 00:09:36 -0300

    change: new background (clear) color

--- Source/UI/DebugUI.h
+++ Source/UI/DebugUI.h
@@ -21,6 +21,6 @@ private:
     
 private:
     
-    inline static ImVec4 mClearColor = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    inline static ImVec4 mClearColor = ImVec4(0.18f, 0.16f, 0.25f, 1.00f);
     inline static float mUIScale = 1.15f;
 };
